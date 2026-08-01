bde6b8735782fe8193909303479a6abc69988a57
FengguaYa <FengguaYa@outlook.com>
2026-08-01 20:15:56 +0800

    feat: restore non-vanilla config UI (Cloth Config) and Sodium 0.9 config pages

--- common/src/main/java/team/teampotato/ruok/mixin/minecraft/gui/OptionsScreenMixin.java
+++ common/src/main/java/team/teampotato/ruok/mixin/minecraft/gui/OptionsScreenMixin.java
@@ -10,10 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import team.teampotato.ruok.config.RuOK;
-import team.teampotato.ruok.gui.vanilla.RuOKScreens;
-import team.teampotato.ruok.gui.vanilla.screen.ListScreen;
-import team.teampotato.ruok.util.ModLoadState;
+import team.teampotato.ruok.gui.RuOKConfigScreen;
 
 @Mixin(OptionsScreen.class)
 public abstract class OptionsScreenMixin extends Screen {
@@ -24,20 +21,10 @@ public abstract class OptionsScreenMixin extends Screen {
 
     @Inject(method = "init()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/HeaderAndFooterLayout;visitWidgets(Ljava/util/function/Consumer;)V"))
     private void onInit(CallbackInfo ci, @Local GridLayout.RowHelper adder) {
-        Button button;
-        if (ModLoadState.isSodium() && !RuOK.get().UseVanillaGui) {
-            // 如果玩家加载了Sodium模块，且不希望使用VanillaGui，显示ListScreen
-            button = Button.builder(
-                    Component.translatable("ruok.options.entity.list"),
-                    (b) -> this.minecraft.setScreen(new ListScreen(Component.translatable("ruok.setting.list"), this, this.minecraft.options))
-            ).build();
-        } else {
-            // 默认或Sodium加载且希望使用RuOK界面，显示RuOKScreens
-            button = Button.builder(
-                    Component.translatable("ruok.options.gui.ruok"),
-                    (b) -> this.minecraft.setScreen(new RuOKScreens(this, this.minecraft.options))
-            ).build();
-        }
+        Button button = Button.builder(
+                Component.translatable("ruok.options.gui.ruok"),
+                (b) -> this.minecraft.setScreen(RuOKConfigScreen.create(this))
+        ).build();
         adder.addChild(button);
     }
 
