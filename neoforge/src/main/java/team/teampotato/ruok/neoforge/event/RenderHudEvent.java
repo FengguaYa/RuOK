e32e1113b160aa5395946d091ed095a9ecd98064
FengguaYa <FengguaYa@outlook.com>
2026-08-01 17:00:42 +0800

    fix: use new EventBusSubscriber API for NeoForge 26.1 (no bus attribute)

--- neoforge/src/main/java/team/teampotato/ruok/neoforge/event/RenderHudEvent.java
+++ neoforge/src/main/java/team/teampotato/ruok/neoforge/event/RenderHudEvent.java
@@ -1,14 +1,16 @@
 package team.teampotato.ruok.neoforge.event;
 
 import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import org.jetbrains.annotations.NotNull;
+import team.teampotato.ruok.RuOKMod;
 import team.teampotato.ruok.util.render.TextRender;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
+@EventBusSubscriber(value = Dist.CLIENT, modid = RuOKMod.MOD_ID)
 public class RenderHudEvent {
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void HudRender(RenderGuiEvent.@NotNull Pre event) { // 方法需要为 static
